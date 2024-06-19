126b2156cbd0a0b265d6daf10d7679c0edc17df3
pulv1593 <jiro0727@naver.com>
2024-06-19 23:30:26 +0900

    fix: reviews/page.tsx suspense 태그 추가 #1

--- src/app/(route)/reviews/page.tsx
+++ src/app/(route)/reviews/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import PostReview from '@/app/_components/review/PostReview'
 
 const PostReviewPage = () => {
   return (
-    <PostReview /> 
+    <Suspense fallback={<div>Loading...</div>}>
+      <PostReview /> 
+    </Suspense>
   )
 }
 
